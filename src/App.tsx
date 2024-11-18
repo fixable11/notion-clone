55d9035eb21007d6cce93b684aeb699b1fd0d90f
Mykyta Borovyk <fixable11@gmail.com>
2024-11-18 17:51:04 +0200

    Added FileImage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import './App.css';
 import { Page } from './Page/Page.tsx';
 import { AppStateProvider } from './state/AppStateContext.tsx';
-import { createPage } from './utils/createPage.ts';
 import { Route, Routes } from 'react-router-dom';
 import { Auth } from './auth/Auth.tsx';
 import { Private } from './auth/Private.tsx';
-
-const initialState = createPage();
 
 function App() {
   return (
